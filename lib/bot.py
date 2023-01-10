2df6b65384b26d9c66c4971bd72d053147d4bcaa
Gabriel Pimenta <gabrielpimenttas@gmail.com>
2023-01-10 12:51:21 -0300

    adding user agent

--- lib/bot.py
+++ lib/bot.py
@@ -20,7 +20,8 @@ betfair_email = getenv("BETFAIR_EMAIL")
 betfair_password = getenv("BETFAIR_PASSWORD")
 headless = getenv("HEADLESS")
 
-user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36"
+# user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36"
+user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.79 Safari/537.36"
 
 def bot(options):
 
